3188f4d00ddb2f7f6879436d92698d62af090ba8
Vladislav <divergenny@gmail.com>
2021-07-27 10:47:26 +0500

    0. Что такое обобщенные типы (generics)

---
+++ src/main/java/ru/job4j/generics/Generics.java
@@ -0,0 +1,70 @@
+package ru.job4j.generics;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Generics {
+    public static void main(String[] args) {
+        Generics gen = new Generics();
+        List<Animal> first = new ArrayList<>();
+        List<Predator> second = new ArrayList<>();
+        List<Tiger> third = new ArrayList<>();
+        first.add(new Animal());
+        second.add(new Predator());
+        third.add(new Tiger());
+
+        gen.printObject(first);
+        gen.printObject(second);
+        gen.printObject(third);
+        System.out.println();
+
+        //gen.printBoundedWildCard(first); Метод вызывающий ошибку компиляции
+        gen.printBoundedWildCard(second);
+        gen.printBoundedWildCard(third);
+        System.out.println();
+
+        gen.printLowerBoundedWildCard(first);
+        gen.printLowerBoundedWildCard(second);
+        // gen.printLowerBoundedWildCard(third); Метод вызывающий ошибку компиляции
+    }
+
+    /**
+     * метод - работает без ограничений, т.е.
+     * в него можно передавать коллекцию,
+     * которая хранит любые типы.
+     * @param list коллекция, которая принимает любые типы
+     */
+    public void printObject(List<?> list) {
+        for (Iterator<?> it = list.iterator(); it.hasNext();) {
+            Object next = it.next();
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+
+    /**
+     * метод - иметь ограничение
+     * сверху и ограничиваться
+     * классом Predator.
+     * @param list коллекция, которая ограничивается сверху(Bounded Wildcard) классом Predator
+     */
+    public void printBoundedWildCard(List<? extends Predator> list) {
+        for (Iterator<? extends Predator> it = list.iterator(); it.hasNext();) {
+            Object next = it.next();
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+
+    /**
+     * метод - имеет ограничение
+     * снизу и ограничиваться
+     * классом Predator.
+     * @param list  коллекция, которая ограничивается снизу(Lower bounded wcard) классом Predator
+     */
+    public void printLowerBoundedWildCard(List<? super Predator> list) {
+        for (Iterator<? super Predator> it = list.iterator(); it.hasNext();) {
+            Object next = it.next();
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+}
