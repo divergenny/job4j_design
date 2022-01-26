8ae524228a9f41369f8ad8b3efcd0350b3c80d35
Vladislav <divergenny@gmail.com>
2022-01-26 21:28:35 +0500

    3. Бот

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -8,6 +8,12 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+
+    private static void sendAnswerFromServer(OutputStream out, String message) throws IOException {
+        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+        out.write(message.getBytes());
+    }
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
@@ -17,7 +23,6 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         if (0 == iterator) {
                             message = str;
@@ -25,9 +30,14 @@ public class EchoServer {
                         }
                         System.out.println(str);
                     }
-                    if (message.matches(".*\\s/\\?msg=Bye\\s.*")) {
-                        System.out.println("The server is shutting down. Bye!");
+                    if (message.matches(".*\\s/\\?msg=Exit\\s.*")) {
+                        sendAnswerFromServer(out, "Shut down the server.");
+                        out.flush();
                         server.close();
+                    } else if (message.matches(".*\\s/\\?msg=Hello\\s.*")) {
+                        sendAnswerFromServer(out, "Hello.");
+                    } else {
+                        sendAnswerFromServer(out, "What.");
                     }
                     out.flush();
                 }
