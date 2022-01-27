2a37cee685e10ffee8adec0115c70e2c1485d992
divergenny <divergenny@gmail.com>
2022-01-27 10:01:52 +0500

    2. Simple Loggin Facade 4 Java.

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -1,10 +1,20 @@
 package ru.job4j.io;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+/**
+ * This class is show how to use Logging.
+ * In this example I am using slf4j, that is
+ * Simple Logging Facade For Java.
+ * This is library that can help to use different logging
+ * instruments for teams.
+ * For example, one team can use LogBack and another log4j throw slf4j.
+ * SLF uses a design pattern - facade. The facade template simplifies
+ * API loggers, so makes them understandable.
+ */
 public class UsageLog4j {
-    private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
         LOG.trace("trace message");
