e066d9d17669d2e0a71a62591596081718a811c5
divergenny <divergenny@gmail.com>
2022-02-13 18:48:08 +0500

    0. Что такое база данных.

---
+++ data/sql/crud_workers_table.sql
@@ -0,0 +1,13 @@
+create table workers(
+	id serial primary key,
+	name varchar(255),
+	married boolean,
+	birthday date
+);
+
+insert into workers(name, married, birthday) 
+values('Vlad', false, '20.01.1997')
+
+update workers set birthday = '22.07.1997'
+
+delete from workers
