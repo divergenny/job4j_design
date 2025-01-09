e6330285474d88ab3df3923cb0fd6060bd6799ec
divergenny <divergenny@gmail.com>
2025-01-09 22:22:12 +0500

    Сортировки docs: description to method

--- src/main/java/ru/job4j/algo/sort/IntervalMerger.java
+++ src/main/java/ru/job4j/algo/sort/IntervalMerger.java
@@ -4,6 +4,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class IntervalMerger {
+
+    /**
+     * Задача 56. Merge Intervals из leetcode.
+     * Дан массив интервалов, где intervals[i] = [start(i), end(i)],
+     * объединяем все перекрывающиеся интервалы и возвращаем массив непересекающихся интервалов,
+     * которые охватывают все интервалы во входных данных.
+     *
+     * @param intervals массив из интервалов
+     * @return итоговый отсортированный массив с перекрывающимися интервалами
+     */
     public int[][] merge(int[][] intervals) {
         if (intervals.length <= 1) {
             return intervals;
